e478a0e955a1822d040f4671d0134752ed5d8fc0
Barbara Friščić <barbarafriscic93@gmail.com>
2023-03-14 11:36:43 +0100

    Console "game" counting numbers towards 100. Try catch block to catch an error, write an error message to User and then restart the app. If used without error, after first attempt user can choose to play again or exit application.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -121,6 +121,84 @@ namespace ConsoleApp1
             String velikiString;
             string maliString;
 
+
+            //Day2
+
+            //string ReverseString2(string str)
+            //{
+            //    return new string(str.Reverse().ToArray()); 
+            //}
+            //Console.WriteLine("Unesite riječ");
+            //string userWord = Console.ReadLine();
+            //Console.WriteLine($"Vaš riječ, ali izokrenuta: {ReverseString2(userWord)}");
+
+            //var haystack_1 = new object[] { '3', "123124234", null, "needle", "world", "hay", 2, '3', true, false };
+            //var haystack_2 = new object[] { "283497238987234", "a dog", "a cat", "some random junk", "a piece of hay", "needle", "something somebody lost a while ago" };
+            //var haystack_3 = new object[] { 1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 5, 4, 3, 4, 5, 6, 67, 5, 5, 3, 3, 4, 2, 34, 234, 23, 4, 234, 324, 324, "needle", 1, 2, 3, 4, 5, 5, 6, 5, 4, 32, 3, 45, 54 };
+
+
+            //string FindNeedle(object[] haystack)
+            //{
+            //   int needlePosition = -1;
+            //   for(int index= 0; index< haystack.Length; index++)
+            //    {                   
+            //        if (haystack[index].ToString() == "needle" )
+            //        {
+            //            needlePosition = index;
+            //        }
+            //    }
+            //    return $"Found the needle at position {needlePosition} ";
+            // }
+            //Console.WriteLine(FindNeedle(haystack_1));
+
+            Console.WriteLine("Unesite broj");
+            try
+            {
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+                if (userNumber < 100)
+                {
+                    while (userNumber <= 100 )
+                    {
+                        Console.WriteLine(userNumber++);
+                    }
+                }
+                else if( userNumber > 100 ) 
+                {
+                    while (userNumber >= 100 )
+                    {
+                        Console.WriteLine(userNumber--);
+                    }
+                }
+                string userInput;
+                do
+                {
+                    Console.WriteLine("Želite li ponovno igrati unesite y, za izlazak iz aplikacije unesite x.");
+                    userInput = Console.ReadLine();
+
+                } while (userInput.ToLower() != "y" && userInput.ToLower() != "x");
+                if( userInput.ToLower() == "y")
+                {
+                    Console.Clear();
+                    Program.Main(args);
+                }
+                else if (userInput.ToLower() == "x") 
+                { 
+                    Console.Clear();
+                    Environment.Exit(0);
+                }                 
+            }
+            catch (Exception)
+            {
+
+                Console.Error.WriteLine("Neispravan unos.");
+                Console.ReadLine();
+                Console.Clear();
+                Program.Main(args);
+            }
+            
+            
+
+
             Console.ReadLine();
         }
     }
