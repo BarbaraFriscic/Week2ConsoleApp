9b81d8a39da9605cc8f4716cc5f9b588a4da5a63
Barbara Friščić <barbarafriscic93@gmail.com>
2023-03-15 14:46:11 +0100

    Trying Data Annotations. Validate Employee object, catch errors.

--- OOPBasics/OOPBasics/Employee.cs
+++ OOPBasics/OOPBasics/Employee.cs
@@ -10,5 +10,10 @@ namespace OOPBasics
     {
         public Guid Id { get; set; }
         public DateTime DateOfEmployment { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id:{Id}\n{FirstName} {LastName}\nEmployment date: {DateOfEmployment}\nAddress: {Address}\nDOB: {DOB}\nEmail: {Email}";
+        }
     }
 }
--- OOPBasics/OOPBasics/Program.cs
+++ OOPBasics/OOPBasics/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,22 @@ namespace OOPBasics
 {
     internal class Program
     {
+        public static void Validate(object model)
+        {
+            string errorMessage = "";
+            List<ValidationResult> results = new List<ValidationResult>();
+            ValidationContext context = new ValidationContext(model);
+            bool isValid = Validator.TryValidateObject(model, context, results, true);
+            if(!isValid)
+            {
+                foreach (ValidationResult result in results) 
+                {
+                    errorMessage += result.ErrorMessage +"\n";
+                }
+                throw new Exception(errorMessage);
+            }
+        }
+
         static void Main(string[] args)
         {
             Location zagreb = new Location();
@@ -33,25 +50,70 @@ namespace OOPBasics
             //firstClient.Address = firstClient.SetAddress("Zagreb"); // nije static metoda
             //Console.WriteLine(firstClient.Address.City.ToString());
 
-            Console.WriteLine("Unesite podatke za novu uslugu");
-            string escKey = "x";
-            List<Service> servicesList = new List<Service>();
-            do
-            {
-                Service newService = new Service();
-                Console.WriteLine("Ime usluge:");
-                newService.Name = Console.ReadLine();
-                Console.WriteLine("Unesite cijenu usluge:");
-                newService.Price = Convert.ToDecimal(Console.ReadLine());
-                servicesList.Add(newService);
-                Console.WriteLine("Ako želite završiti unos pritisnite x. Za nastavk unosa pritisnite bilo koju tipku.");
-                escKey = Console.ReadLine();
-            } while (escKey != "x");
+            //Console.WriteLine("Unesite podatke za novu uslugu");
+            //string escKey = "x";
+            //List<Service> servicesList = new List<Service>();
+            //do
+            //{
+            //    Service newService = new Service();
+            //    Console.WriteLine("Ime usluge:");
+            //    newService.Name = Console.ReadLine();
+            //    Console.WriteLine("Unesite cijenu usluge:");
+            //    newService.Price = Convert.ToDecimal(Console.ReadLine());
+            //    servicesList.Add(newService);
+            //    Console.WriteLine("Ako želite završiti unos pritisnite x. Za nastavk unosa pritisnite bilo koju tipku.");
+            //    escKey = Console.ReadLine();
+            //} while (escKey != "x");
 
-            foreach (Service service in servicesList)
+            //foreach (Service service in servicesList)
+            //{
+            //    Console.WriteLine(service.ToString());
+            //}
+            
+            Employee firstEmployee = new Employee
             {
-                Console.WriteLine(service.ToString());
+                Id = Guid.NewGuid(),
+                DateOfEmployment = DateTime.Now,
+                DOB = new DateTime(1993, 12, 27),
+                
+            };
+            try
+            {
+                Console.WriteLine("Unesite ime zaposlenika:");
+                firstEmployee.FirstName = Console.ReadLine();
+
+
+                Console.WriteLine("Unesite prezime zaposlenika:");
+                firstEmployee.LastName = Console.ReadLine();
+
+
+                Console.WriteLine("Unesite email zaposlenika");
+                firstEmployee.Email = Console.ReadLine();
+                Validate(firstEmployee);
+
+                Console.Write(firstEmployee.ToString());
+                
             }
+            catch (Exception ex)
+            {
+
+                Console.Error.WriteLine(ex.Message);
+                Console.ReadLine();
+                Console.Clear();
+               // Program.Main(args);
+            }
+            //Console.WriteLine("Unesite ime zaposlenika:");
+            //firstEmployee.FirstName = Console.ReadLine();
+            //Console.WriteLine("Unesite prezime zaposlenika:");
+            //firstEmployee.LastName = Console.ReadLine();
+            //Console.WriteLine("Unesite email zaposlenika");
+            //firstEmployee.Email = Console.ReadLine();
+
+            Console.Write(firstEmployee.ToString());
+
+            // Person person = new Person(); //Ne može instancirati objekt apstraktne klase
+            
+
 
             Console.ReadKey();
         }
